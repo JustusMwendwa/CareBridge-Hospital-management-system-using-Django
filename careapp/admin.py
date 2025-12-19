163159bc0b15509a6451d2ba779bc12e56cd8fb9
JustusMwendwa <justusmwendwa022@gmail.com>
2025-12-19 09:17:36 +0300

    update

--- careapp/admin.py
+++ careapp/admin.py
@@ -8,3 +8,6 @@ admin.site.register(MedicalRecords)
 admin.site.register(Appointments)
 
 
+
+
+
--- careapp/urls.py
+++ careapp/urls.py
@@ -28,5 +28,15 @@ urlpatterns = [
 
     path('login/', views.login_user,name="login"),
 
+    #Mpesa URLS
+
+   #Mpesa URLS
+    path('pay/', views.pay, name='pay'),
+
+    path('stk/', views.stk, name='stk'),
+    path('token/', views.token, name='token'),
+    path('payment-result/', views.payment_result, name='payment_result'),
+    path('transactions/', views.transactions_list, name='transactions'),
+
 
 ]
